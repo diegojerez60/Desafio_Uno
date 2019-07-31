b27e4d568ea1ce67e16f16e454c728b4625b3e87
diegojerez60 <diegojerez32@gmail.com>
2019-07-31 17:25:05 -0400

    cambios en configuracion para pruebas de despliege como war

--- src/main/java/cl/diegojerez/previred/desafio/SpringbootServicioDesafio2Application.java
+++ src/main/java/cl/diegojerez/previred/desafio/SpringbootServicioDesafio2Application.java
@@ -2,14 +2,26 @@ package cl.diegojerez.previred.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableCircuitBreaker
 @SpringBootApplication
-public class SpringbootServicioDesafio2Application {
+@ComponentScan("cl.diegojerez.previred.*")
+@EnableWebMvc
+public class SpringbootServicioDesafio2Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioDesafio2Application.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootServicioDesafio2Application.class);
+	}
+
+	
 }
